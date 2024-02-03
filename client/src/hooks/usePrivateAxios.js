2f0739d245dfbbb8d25fc289522e9186d3dc4c67
osama aboajeb <osama.aboajeb333@gmail.com>
2024-02-03 22:51:53 +0300

    server only files

--- client/src/hooks/usePrivateAxios.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { PrivateAxiosContext } from "../Context/PrivateAxiosContext";
-const usePrivateAxios = () => {
-  const privateAxios = useContext(PrivateAxiosContext)
-  return (privateAxios)
-}
-
-export default usePrivateAxios;
--- client/src/pages/About/AboutUs.jsx
+++
@@ -1,9 +0,0 @@
-const AboutUs = () => {
-  return (
-    <div className="min-h-calc(100vh-64px) flex items-center justify-center">
-{/*  */}
-    </div>
-  );
-}
-
-export default AboutUs;
